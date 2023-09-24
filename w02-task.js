a1e828dbf33ff34bf59e97cc09ebdad45bf6b599
oliviasmartt <sma21005@byui.edu>
2023-09-23 23:30:27 -0600

    changed

--- w02-task.js
+++ w02-task.js
@@ -1,5 +1,4 @@
 /* W02-Task - Profile Home Page */
-<link rel="stylesheet" type="css" href="main.css"></link>
 
 /* Step 1 - Setup type tasks - no code required */
 
@@ -11,22 +10,18 @@ let profilePicture = "me.jpeg";
 /* Step 3 - Element Variables */
 
 const nameElement = document.getElementById('name');
-const foodElement = document.getElementById('food');
 const yearElement = document.querySelector('#year');
 const imageElement = document.querySelector('image');
 
 
 /* Step 4 - Adding Content */
 
-nameElement.innerHTML = '<strong>${fullName}</strong>';
+nameElement.innerHTML = `<strong>${fullName}</strong>`;
 imageElement.setAttribute('src',profilePicture);
 
 
 
 
 /* Step 5 - Array */
-
-let favFoods = ["Pizza", "Sushi", "Burgers", "Ice Cream"];
-let newFavoriteFood = "Tacos";
-favoriteFoods.push(newFavoriteFood);
-foodElement.innerHTML += '<br>${favFoods}';
+const favoriteFoods = ["Pizza", "Sushi", "Burgers", "Ice Cream"];
+foodElement.innerHTML += `<br>${favoriteFoods}`;
